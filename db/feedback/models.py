bb0849c7995f5a3c58008eee63ea1897ddc2817f
Martin Pontius <m.pontius@52north.org>
2020-11-20 18:29:33 +0100

    Update feedback form

--- db/feedback/models.py
+++ db/feedback/models.py
@@ -1,7 +1,69 @@
 from django.db import models
+import pystache
+from datetime import datetime
 
 
 class Feedback(models.Model):
+    rank_info_consumer = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Information about consumers and consumers profile",
+    )
+    rank_info_storage = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Information on storage points",
+    )
+    rank_info_producer_seller = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Information on local food producers/sellers ",
+    )
+    rank_info_production_service = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Information on production service ",
+    )
+    rank_info_energy_usage = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Evaluations of energy usage",
+    )
+    rank_info_water_supplier = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Information about water supplier",
+    )
+    rank_feature_see_producer_seller = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Access to information on local food producers/sellers",
+    )
+    rank_feature_see_water_supplier = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Access to information on water supplier",
+    )
+    rank_feature_add_producer_seller = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Possibility to add a local food producer/seller on the map",
+    )
+    rank_feature_see_energy_usage = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Access to information on energy usage",
+    )
+    rank_feature_add_by_others = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Possibility to see the information added by other users",
+    )
+    rank_feature_add_comment = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Leave a comment (open question)",
+    )
     usability = models.CharField(
         choices=(
             ("strong_agree", "I strongly agree"),
@@ -13,7 +75,7 @@ class Feedback(models.Model):
         max_length=15,
         null=True,
         blank=True,
-        verbose_name="The application is useful",
+        verbose_name="The application is easy to use",
     )
     attractiveness = models.CharField(
         choices=(
@@ -54,16 +116,29 @@ class Feedback(models.Model):
         blank=True,
         verbose_name="Instruction on how to use the application is easy to understand",
     )
-    main_attraction = models.TextField(
+    usefulness = models.CharField(
+        choices=(
+            ("definitely_yes", "Definitely yes"),
+            ("rather_yes", "Rather yes"),
+            ("no_opinion", "I have no opinion"),
+            ("rather_not", "Rather not"),
+            ("definitely_not", "Definitely not"),
+        ),
+        max_length=14,
         null=True,
         blank=True,
-        verbose_name="What was the main attraction?",
+        verbose_name="Do you think that the data collected through the application can be useful for a better representation of local producers and small gardeners?",
     )
-    other_function = models.TextField(
+    comment = models.TextField(
         null=True,
         blank=True,
         verbose_name="What other functions should the application have?",
     )
+
+    wq_label_template = "Feedback from/de la " + datetime.today().strftime('%Y-%m-%d')
+
+    def __str__(self):
+        return pystache.render(self.wq_label_template, self)
 
     class Meta:
         verbose_name = "feedback"
