f2e0785c8058cbe124df69e2df2e9ee3b39fda13
Martin Pontius <m.pontius@52north.org>
2021-06-14 12:48:37 +0200

    Add new map icons for producers

--- app/js/TulceaTool/main.js
+++ app/js/TulceaTool/main.js
@@ -17,6 +17,13 @@ map.createIcon("storagepoint", {'iconUrl': config.router.base_url.concat("/image
 map.createIcon("watersupplier", {'iconUrl': config.router.base_url.concat("/images/watersupplier.png"), 'iconSize': [20, 30]});
 map.createIcon("energysource", {'iconUrl': config.router.base_url.concat("/images/energysource.png"), 'iconSize': [20, 30]});
 map.createIcon("productionservice", {'iconUrl': config.router.base_url.concat("/images/productionservice.png"), 'iconSize': [30, 30]});
+map.createIcon("bakery", {'iconUrl': config.router.base_url.concat("/images/bakery.png"), 'iconSize': [35, 35]});
+map.createIcon("fish", {'iconUrl': config.router.base_url.concat("/images/fish.png"), 'iconSize': [40, 40]});
+map.createIcon("group-of-fruits", {'iconUrl': config.router.base_url.concat("/images/group-of-fruits.png"), 'iconSize': [35, 35]});
+map.createIcon("group-of-vegetables", {'iconUrl': config.router.base_url.concat("/images/group-of-vegetables.png"), 'iconSize': [35, 35]});
+map.createIcon("honey", {'iconUrl': config.router.base_url.concat("/images/honey.png"), 'iconSize': [30, 30]});
+map.createIcon("milk-bottle2", {'iconUrl': config.router.base_url.concat("/images/milk-bottle2.png"), 'iconSize': [20, 35]});
+map.createIcon("steak-rare", {'iconUrl': config.router.base_url.concat("/images/steak-rare.png"), 'iconSize': [30, 25]});
 
 config.presync = presync;
 config.postsync = postsync;
